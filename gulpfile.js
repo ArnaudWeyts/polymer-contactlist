e33191ca87603d0e6db32d75960707f16ac519fc
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-08-22 10:59:07 +0200

    updated gulpfile and dependencies, slight update to layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,9 @@
 'use strict';
 
-var gulp 					= require('gulp'),
-		autoprefixer 	= require('gulp-autoprefixer'),
-		cssnano				= require('gulp-cssnano'),
-		browserify		= require('gulp-browserify'),
-		uglify				= require('gulp-uglify'),
-		jshint				= require('gulp-jshint'),
-		composer			= require('composer');
+var gulp = require('gulp');
 
 var SRC = './src';
 var DIST = './dist';
-
-gulp.task('css', function () {
-  return gulp.src(SRC + '/scss/styles.scss')
-    .pipe(autoprefixer({
-    	browsers: ['>1%'],
-    	cascade: false
-    }))
-    .pipe(cssnano())
-    .pipe(gulp.dest(DIST + '/css'));
-});
-
-gulp.task('scripts',function () {
-  return gulp.src(SRC + '/js/*.js')
-  	.pipe(jshint('.jshintrc'))
-  	.pipe(jshint.reporter('jshint-stylish'))
-  	.pipe(browserify({
-  		insertGlobals : true,
-  	}))
-  	.pipe(uglify())
-  	.pipe(gulp.dest(DIST + '/js'));
-
-});
 
 var filesToCopy = [
 		SRC + '/*.html',
@@ -47,14 +19,7 @@ gulp.task('copy',() => {
   	.pipe(gulp.dest(DIST));
 });
 
-gulp.task('watch', ['default'], function ()  {
-  gulp.watch(SRC + '/css/**/*.css', ['css']);
-  gulp.watch(filesToCopy, ['copy']);
-  gulp.watch(SRC + '/img/*', ['copy']);
-  gulp.watch(SRC + '/js/**/*.js', ['scripts']);
-});
-
-gulp.task('default', ['copy','css','scripts']);
+gulp.task('default', ['copy', 'connect']);
 
 
 
